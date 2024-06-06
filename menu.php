9e07fba6c35323d600dd6e6391884f34cdeac613
LenaPignolet <lena.pignolet@etudiant.univ-reims.fr>
2024-06-06 13:50:49 +0000

    fichier config pour lien menu

--- menu.php
+++ menu.php
@@ -1,7 +1,8 @@
+<?php require('config.php'); ?>
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
   <div class="flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="index.php" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="images/logo.png" class="h-8" alt="Green Garden Logo" />
+      <img src="<?php echo BASE_URL; ?>images/logo.png" class="h-8" alt="Green Garden Logo" />
       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Green Garden</span>
   </a>
   
@@ -9,8 +10,8 @@
       <?php
         if (!isset($_SESSION['id'])) {
           ?>
-      <a class="connection_button" href="auth/formConnexion.php">Connexion</a>
-      <a class="connection_button" href="auth/formInscription.php">Inscription</a>
+      <a class="connection_button" href="<?php echo BASE_URL; ?>auth/formConnexion.php">Connexion</a>
+      <a class="connection_button" href="<?php echo BASE_URL; ?>auth/formInscription.php">Inscription</a>
 
       <?php
         }else {?>
@@ -22,7 +23,7 @@
       <?php
             }
             ?>
-      <a href="admin/gestion.php"><img id="logo_admin" src="images/admin.png" alt="Logo admin" class="h-6 w-6"></a>
+      <a href="<?php echo BASE_URL; ?>admin/gestion.php"><img id="logo_admin" src="<?php echo BASE_URL; ?>images/admin.png" alt="Logo admin" class="h-6 w-6"></a>
       
       <!-- Dropdown menu -->
       
@@ -34,10 +35,10 @@
         </div>
         <ul class="py-2" aria-labelledby="user-menu-button">
           <li>
-            <a href="admin/Usagers/profil.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil</a>
+            <a href="<?php echo BASE_URL; ?>admin/Usagers/profil.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil</a>
           </li>
           <li>
-            <a href="auth/logout.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Se déconnecter</a>
+            <a href="<?php echo BASE_URL; ?>auth/logout.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Se déconnecter</a>
           </li>
         </ul>
       </div>
@@ -54,16 +55,16 @@
   <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
     <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <li>
-        <a href="index.php" class="block py-2 px-3 text-white bg-green-600 rounded md:bg-transparent md:text-green-700 md:p-0 md:dark:text-blue-500" aria-current="page">Accueil</a>
+        <a href="<?php echo BASE_URL; ?>index.php" class="block py-2 px-3 text-white bg-green-600 rounded md:bg-transparent md:text-green-700 md:p-0 md:dark:text-blue-500" aria-current="page">Accueil</a>
       </li>
       <li>
-        <a href="liste_jardins.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Jardins</a>
+        <a href="<?php echo BASE_URL; ?>liste_jardins.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Jardins</a>
       </li>
       <li>
         <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Qui sommes-nous ?</a>
       </li>
       <li>
-        <a href="contact.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+        <a href="<?php echo BASE_URL; ?>contact.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
       </li>
     </ul>
   </div>
