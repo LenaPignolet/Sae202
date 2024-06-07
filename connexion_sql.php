565ce692dc52cbffe1bb35b6443a64dff0a5e0ee
Loanngerme <germeloann@gmail.com>
2024-06-07 11:48:14 +0200

    Update connexion_sql.php

--- connexion_sql.php
+++ connexion_sql.php
@@ -1,7 +1,15 @@
 <?php
-    try {
-        $mabd = new PDO('mysql:host=localhost;dbname=sae202Base;charset=UTF8;','usersae202', 'sae202');
-    } catch (\PDOException $e) {
-        echo $e->getMessage();
-    }
+$host = 'localhost';
+$dbname = 'sae202';
+$username = 'sae202User';
+$password = '123';
+
+try {
+    $mabd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    // Configurer PDO pour qu'il gÃ©nÃ¨re des exceptions en cas d'erreurs
+    $mabd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Ã‰chec de la connexion : ' . $e->getMessage();
+    exit;
+}
 ?>
