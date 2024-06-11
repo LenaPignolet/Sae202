a9b8e4aeaf35ef3c89594c24c84cf16e0971112c
Basheer El Kurdi <112487394+basheer-11@users.noreply.github.com>
2024-06-11 16:09:57 +0200

    Update supprimer_jardin.php dashboard

--- admin/Jardins/supprimer_jardin.php
+++ admin/Jardins/supprimer_jardin.php
@@ -1,24 +1,90 @@
-<!DOCTYPE html>
+<!doctype html>
+<!-- 
+* Bootstrap Simple Admin Template
+* Version: 2.1
+* Author: Alexis Luna
+* Website: https://github.com/alexis-luna/bootstrap-simple-admin-template
+-->
 <html lang="fr">
+
 <head>
-<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Supprimer un jardin</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
-    <link rel="icon" type="image/png" href="../images/icon.jpg">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Page admin">
+    <title>Dashboard</title>
+    <link href="../assets/vendor/fontawesome/css/fontawesome.min.css" rel="stylesheet">
+    <link href="../assets/vendor/fontawesome/css/solid.min.css" rel="stylesheet">
+    <link href="../assets/vendor/fontawesome/css/brands.min.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendor/datatables/datatables.min.css" rel="stylesheet">
+    <link href="../assets/css/master.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="icon" type="image/png" href="/images/logo.png">
 </head>
 <body>
+  
+<div class="wrapper">
+        <nav id="sidebar" class="active">
+            <div class="sidebar-header">
+                <img src="../assets/img/logo.png" alt="bootraper logo" width="40px" class="app-logo">
+            </div>
+            <ul class="list-unstyled components text-secondary">
+                <li>
+                    <a href="/admin/gestion.php"><i class="fas fa-home"></i> Dashboard</a>
+                </li>
 
-<?php require('menu.php'); ?>
+                <li>
+                    <a href="gestion_jardin.php"><i class="fas fa-tree"></i> Gestion Jardins</a>
+                </li>
 
-<a href="gestion_jardin.php">retour au tableau de bord</a> 	
-<hr> <h1>Suppression</h1> <hr>
+                <li>
+                    <a href="../Parcelle/gestion_parcelle.php"><i class="fas fa-chess-board"></i> Gestion Parcelles</a>
+                </li>
+
+                <li>
+                    <a href="./Usagers/user_gestion.php"><i class="fas fa-user-friends"></i> Gestion Users</a>
+                </li>
+
+                <li>
+                    <a href="/index.php"><i class="fas fa-arrow-left"></i> Retour</a>
+                </li>
+
+            </ul>
+        </nav>
+        <div id="body" class="active">
+            <!-- navbar navigation component -->
+            <nav class="navbar navbar-expand-lg navbar-white bg-white">
+                <button title="menu" type="button" id="sidebarCollapse" class="btn btn-light">
+                    <i class="fas fa-bars"></i><span></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="nav navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <div class="nav-dropdown">
+                                <div class="dropdown-menu dropdown-menu-end nav-link-menu" aria-labelledby="nav1">
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+<div class="content">
+    <div class="container">
+          <div class="page-title">
+              <h3>Vous venez de supprimer un jardin</h3>
+          </div>
+          <div class="box box-primary">
+                        <div class="box-body">
+                            <div class="tab-content" id="myTabContent">
+                                <div class="tab-pane fade active show" id="general" role="tabpanel" aria-labelledby="general-tab">
+                                    <div class="col-md-6">
 
 <?php
 // recupérer dans l'url l'id de l'album à supprimer
 $allJardinId = $_GET['num'];
 
-$mabd = new PDO('mysql:host=localhost;dbname=sae202;charset=UTF8;', 'Usersae202', '123');
+$mabd = new PDO('mysql:host=localhost;dbname=sae202Base;charset=UTF8;', 'Usersae202', '123');
 $mabd->query('SET NAMES utf8;');
 
 // tapez ici la requete de suppression de l'album dont l'id est passé dans l'url
@@ -26,12 +92,19 @@ $req = 'DELETE FROM Jardin WHERE jardin_id = ' . $allJardinId;
  
 $resultat = $mabd->query($req);
 
-echo '<h2>Vous venez de supprimer le jardin numéro ' . $allJardinId . '</h2>';
+echo '<p>Suppression du jardin réussie ! <i class="fas fa-circle-check" style="color: #037c58;"></i></p>';
+echo "<br>";
+echo '<a  class="btn btn-success" href="gestion_jardin.php">Retour au tableau de bord</a>';
 ?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<script src="../assets/vendor/jquery/jquery.min.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/script.js"></script>
 </body>
-<footer>
-    <p>© PAGEC - Tous droits réservés</p>
-    <a href="../mentions.php">Mentions légales</a>
-</footer>
+
 </html>
