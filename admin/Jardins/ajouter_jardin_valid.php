3870c11e06de13294ad5eff7c671fd3a7956ffa1
Basheer El Kurdi <112487394+basheer-11@users.noreply.github.com>
2024-06-11 16:06:13 +0200

    Update ajouter_jardin_valid.php dashboard

--- admin/Jardins/ajouter_jardin_valid.php
+++ admin/Jardins/ajouter_jardin_valid.php
@@ -1,73 +1,171 @@
-<!DOCTYPE html>
+<!doctype html>
+<!-- 
+* Bootstrap Simple Admin Template
+* Version: 2.1
+* Author: Alexis Luna
+* Website: https://github.com/alexis-luna/bootstrap-simple-admin-template
+-->
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Validation</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Page admin">
+    <title>Dashboard</title>
+    <link href="../assets/vendor/fontawesome/css/fontawesome.min.css" rel="stylesheet">
+    <link href="../assets/vendor/fontawesome/css/solid.min.css" rel="stylesheet">
+    <link href="../assets/vendor/fontawesome/css/brands.min.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendor/datatables/datatables.min.css" rel="stylesheet">
+    <link href="../assets/css/master.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="icon" type="image/png" href="/images/logo.png">
 </head>
 <body>
   
-<?php require('menu.php'); ?>
+<div class="wrapper">
+        <nav id="sidebar" class="active">
+            <div class="sidebar-header">
+                <img src="../assets/img/logo.png" alt="bootraper logo" width="40px" class="app-logo">
+            </div>
+            <ul class="list-unstyled components text-secondary">
+                <li>
+                    <a href="/admin/gestion.php"><i class="fas fa-home"></i> Dashboard</a>
+                </li>
 
-<a href="jardin_gestion.php">Retour au tableau de bord</a>
-<hr>
+                <li>
+                    <a href="gestion_jardin.php"><i class="fas fa-tree"></i> Gestion Jardins</a>
+                </li>
 
-<h1 class="valide">Gestion de nos jardins</h1>
-<h2 class="valide">Vous venez d'ajouter un jardin</h2>
-<hr>
+                <li>
+                    <a href="../Parcelle/gestion_parcelle.php"><i class="fas fa-chess-board"></i> Gestion Parcelles</a>
+                </li>
+
+                <li>
+                    <a href="./Usagers/user_gestion.php"><i class="fas fa-user-friends"></i> Gestion Users</a>
+                </li>
+
+                <li>
+                    <a href="/index.php"><i class="fas fa-arrow-left"></i> Retour</a>
+                </li>
+
+            </ul>
+        </nav>
+        <div id="body" class="active">
+            <!-- navbar navigation component -->
+            <nav class="navbar navbar-expand-lg navbar-white bg-white">
+                <button title="menu" type="button" id="sidebarCollapse" class="btn btn-light">
+                    <i class="fas fa-bars"></i><span></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="nav navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <div class="nav-dropdown">
+                                <div class="dropdown-menu dropdown-menu-end nav-link-menu" aria-labelledby="nav1">
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+<div class="content">
+    <div class="container">
+          <div class="page-title">
+              <h3>Vous venez d'ajouter un jardin</h3>
+          </div>
+          <div class="box box-primary">
+                        <div class="box-body">
+                            <div class="tab-content" id="myTabContent">
+                                <div class="tab-pane fade active show" id="general" role="tabpanel" aria-labelledby="general-tab">
+                                    <div class="col-md-6">
+
 
 <?php
-// Vérifiez si les données POST existent
-if(isset($_POST['nom'], $_POST['adresse'], $_POST['surface'], $_POST['numparcelle'])) {
-    // Récupération des données passées par le formulaire
-    $nom = $_POST['nom'];
-    $adresse = $_POST['adresse'];
-    $surface = $_POST['surface'];
-    $numparcelle = $_POST['numparcelle'];
+$nom = $_POST['nom'];
+$adresse = $_POST['adresse'];
+$surface = $_POST['surface'];
+$nParcelle = $_POST['nParcelle'];
 
-    // Traitement du téléchargement de la photo
-    $photo = '';
-    if (isset($_FILES['photo']) && $_FILES['photo']['error'] === 0) {
-        $photo = basename($_FILES["photo"]["name"]);
-        $target_dir = "../images/uploads/";
-        $target_file = $target_dir . $photo;
+try {
+    $mabd = new PDO('mysql:host=localhost;dbname=sae202Base;charset=UTF8;', 'Usersae202', '123');
+    $mabd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $mabd->query('SET NAMES utf8;');
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+    die();
+}
 
-        if (!move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
-            echo "Une erreur s'est produite lors du téléchargement de la photo.";
-            exit;
-        }
-    }
+// Vérification du format de l'image téléchargée
+$imageType = $_FILES["photo"]["type"];
+$allowedTypes = ["image/png", "image/jpg", "image/jpeg"];
+if (!in_array($imageType, $allowedTypes)) {
+    echo '<p>Désolé, le type d\'image n\'est pas reconnu ! Seuls les formats PNG et JPEG sont autorisés.</p>';
+    echo "<br>";
+    echo '<a  class="btn btn-success" href="gestion_jardin.php">Retour au tableau de bord</a>';
+    die();
+}
 
-    try {
-        // Connexion à la base de données
-        $mabd = new PDO('mysql:host=localhost;dbname=sae202;charset=UTF8;', 'Usersae202', '123');
-        $mabd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Création d'un nouveau nom pour cette image téléchargée pour éviter d'avoir 2 fichiers avec le même nom
+$nouvelleImage = date("Y_m_d_H_i_s") . "---" . basename($_FILES["photo"]["name"]);
+$targetDir = "/var/www/sae202/images/uploads/"; // Chemin correct vers le dossier
 
-        // Préparation de la requête SQL
-        $req = $mabd->prepare('INSERT INTO Jardin (jardin_photo, jardin_nom, jardin_coord, jardin_surface, parcelle_id) VALUES (:photo, :nom, :adresse, :surface, :numparcelle)');
-        
-        // Exécution de la requête
-        $req->bindParam(':photo', $photo);
-        $req->bindParam(':nom', $nom);
-        $req->bindParam(':adresse', $adresse);
-        $req->bindParam(':surface', $surface);
-        $req->bindParam(':numparcelle', $numparcelle);
+// Vérification si le dossier existe et est accessible
+if (!is_dir($targetDir)) {
+    echo '<p>Le dossier cible n\'existe pas. Veuillez vérifier le chemin.</p>';
+    echo "<br>";
+    echo '<a  class="btn btn-success" href="gestion_jardin.php">Retour au tableau de bord</a>';
+    die();
+}
 
-        $req->execute();
+$targetFilePath = $targetDir . $nouvelleImage;
 
-        echo 'Le jardin a été ajouté avec succès.';
-    } catch(PDOException $e) {
-        echo "Erreur lors de l\'ajout du jardin: " . $e->getMessage();
+// Dépôt du fichier téléchargé dans le dossier
+if (is_uploaded_file($_FILES["photo"]["tmp_name"])) {
+    if (!move_uploaded_file($_FILES["photo"]["tmp_name"], $targetFilePath)) {
+        echo '<p>Problème avec la sauvegarde de l\'image, désolé...</p>';
+        echo "<br>";
+        echo '<a  class="btn btn-success" href="gestion_jardin.php">Retour au tableau de bord</a>';
+        die();
     }
 } else {
-    echo "Tous les champs du formulaire doivent être remplis.";
+    echo '<p>Problème : image non chargée...</p>';
+    echo "<br>";
+    echo '<a  class="btn btn-success" href="gestion_jardin.php">Retour au tableau de bord</a>';
+    die();
 }
+
+// Préparation de la requête d'insertion
+$req = $mabd->prepare('INSERT INTO Jardin (jardin_photo, jardin_nom, jardin_coord, jardin_surface, jardin_n_parcelle) VALUES (:photo, :nom, :adresse, :surface, :nParcelle)');
+
+// Exécution de la requête avec les valeurs passées en paramètres
+try {
+    $resultat = $req->execute([
+        ':photo' => $nouvelleImage,
+        ':nom' => $nom,
+        ':adresse' => $adresse,
+        ':surface' => $surface,
+        ':nParcelle' => $nParcelle
+    ]);
+    echo '<p>Jardin ajouté avec succès !  <i class="fas fa-circle-check" style="color: #037c58;"></i></p>';
+    echo "<br>";
+    echo '<a  class="btn btn-success" href="gestion_jardin.php">Retour au tableau de bord</a>';
+} catch (PDOException $e) {
+    echo '<p>Échec de l\'ajout du jardin : ' . $e->getMessage() . '</p>';
+    echo "<br>";
+    echo '<a  class="btn btn-success" href="gestion_jardin.php">Retour au tableau de bord</a>';
+}
+
 ?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<script src="../assets/vendor/jquery/jquery.min.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/script.js"></script>
 </body>
-<footer>
-    <p>© PAGEC - Tous droits réservés</p>
-    <a href="../mentions.php">Mentions légales</a>
-</footer>
+
 </html>
