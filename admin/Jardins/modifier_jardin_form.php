1da133a28cadde8ce948925613015aee9ec3b236
Basheer El Kurdi <112487394+basheer-11@users.noreply.github.com>
2024-06-11 23:01:20 +0200

    Update modifier_jardin_form.php dashboard

--- admin/Jardins/modifier_jardin_form.php
+++ admin/Jardins/modifier_jardin_form.php
@@ -20,9 +20,10 @@
     <link href="../assets/css/master.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="/images/logo.png">
 </head>
+
 <body>
-  
-<div class="wrapper">
+
+    <div class="wrapper">
         <nav id="sidebar" class="active">
             <div class="sidebar-header">
                 <img src="../assets/img/logo.png" alt="bootraper logo" width="40px" class="app-logo">
@@ -68,7 +69,7 @@
                 </div>
             </nav>
             <!-- end of navbar navigation component -->
-<div class="content">
+            <div class="content">
                 <div class="container">
                     <div class="page-title">
                         <h3>Modification d'un jardin</h3>
@@ -78,75 +79,75 @@
                             <div class="tab-content" id="myTabContent">
                                 <div class="tab-pane fade active show" id="general" role="tabpanel" aria-labelledby="general-tab">
                                     <div class="col-md-6">
-                                        
-<?php
-if(isset($_GET['num'])) {
-    $allJardinId = $_GET['num'];
 
-    try {
-        $mabd = new PDO('mysql:host=localhost;dbname=sae202Base;charset=UTF8;', 'Usersae202', '123');
-        $mabd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-        $req = "SELECT * FROM Jardin WHERE jardin_id = :allJardinId";
-        $stmt = $mabd->prepare($req);
-        $stmt->execute(['allJardinId' => $allJardinId]);
+                                        <?php
+                                        if (isset($_GET['num'])) {
+                                            $allJardinId = $_GET['num'];
 
-        if($stmt->rowCount() > 0) {
-            $allJardin = $stmt->fetch(PDO::FETCH_ASSOC);
-            $req_parcelle = "SELECT * FROM Parcelle";
-            $resultat_parcelle = $mabd->query($req_parcelle);
+                                            try {
+                                                $mabd = new PDO('mysql:host=localhost;dbname=sae202Base;charset=UTF8;', 'Usersae202', '123');
+                                                $mabd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                                                $req = "SELECT * FROM Jardin WHERE jardin_id = :allJardinId";
+                                                $stmt = $mabd->prepare($req);
+                                                $stmt->execute(['allJardinId' => $allJardinId]);
 
-            echo '<form method="POST" action="modifier_jardin_valid.php" enctype="multipart/form-data">';
-            // Champ caché pour stocker l'ancien nom de fichier photo
-            echo '<input type="hidden" name="nouvelle_photo_old" value="' . htmlspecialchars($allJardin['jardin_photo']) . '">';
-            // Champ hidden pour l'ID du jardin
-            echo '<input type="hidden" name="num" value="' . htmlspecialchars($allJardin['jardin_id']) . '">';
-            // Champ pour la nouvelle photo
-            echo '<div class="mb-3">
-                    <label class="form-label">Nouvelle Photo</label>
-                    <input id="formFile1" class="form-control" type="file" name="nouvelle_photo">
-                  </div> 
-                  <br>';
+                                                if ($stmt->rowCount() > 0) {
+                                                    $allJardin = $stmt->fetch(PDO::FETCH_ASSOC);
+                                                    $req_parcelle = "SELECT * FROM Parcelle";
+                                                    $resultat_parcelle = $mabd->query($req_parcelle);
 
-            echo '<div class="mb-3"> <label for="site-title" class="form-label">Le nom du jardin</label>
-                  <input class="form-control" type="text" name="nom" value="' . htmlspecialchars($allJardin['jardin_nom']) . '">
-                  </div><br>';
+                                                    echo '<form method="POST" action="modifier_jardin_valid.php" enctype="multipart/form-data">';
+                                                    // Champ caché pour stocker l'ancien nom de fichier photo
+                                                    echo '<input type="hidden" name="nouvelle_photo_old" value="' . htmlspecialchars($allJardin['jardin_photo']) . '">';
+                                                    // Champ hidden pour l'ID du jardin
+                                                    echo '<input type="hidden" name="num" value="' . htmlspecialchars($allJardin['jardin_id']) . '">';
+                                                    // Champ pour la nouvelle photo
+                                                    echo '<div class="mb-3">
+                                                    <label class="form-label">Nouvelle Photo</label>
+                                                    <input id="formFile1" class="form-control" type="file" name="nouvelle_photo">
+                                                    </div> 
+                                                    <br>';
 
-            echo '<div class="mb-3"> <label for="site-title" class="form-label">Adresse</label>
-                  <input class="form-control" type="text" name="adresse" value="' . htmlspecialchars($allJardin['jardin_coord']) . '">
-                  </div><br>';
+                                                    echo '<div class="mb-3"> <label for="site-title" class="form-label">Le nom du jardin</label>
+                                                        <input class="form-control" type="text" name="nom" value="' . htmlspecialchars($allJardin['jardin_nom']) . '">
+                                                        </div><br>';
 
-            echo '<div class="mb-3"> <label for="site-title" class="form-label">Surface (m²)</label>
-                  <input class="form-control" type="text" name="surface" value="' . htmlspecialchars($allJardin['jardin_surface']) . '">
-                  </div><br>';
+                                                    echo '<div class="mb-3"> <label for="site-title" class="form-label">Adresse</label>
+                                                        <input class="form-control" type="text" name="adresse" value="' . htmlspecialchars($allJardin['jardin_coord']) . '">
+                                                        </div><br>';
 
-            echo '<div class="mb-3"> <label for="site-title" class="form-label">Nombre de parcelles</label>
-                  <input class="form-control" type="text" name="jardin_n_parcelle" value="' . htmlspecialchars($allJardin['jardin_n_parcelle']) . '">
-                  </div><br>';
+                                                    echo '<div class="mb-3"> <label for="site-title" class="form-label">Surface (m²)</label>
+                                                        <input class="form-control" type="text" name="surface" value="' . htmlspecialchars($allJardin['jardin_surface']) . '">
+                                                        </div><br>';
 
-            echo '<div class="mb-3 text-end">
-                  <input class="btn btn-success" type="submit" value="Modifier">
-                  </div>';
-            echo '</form>';
-        } else {
-            echo '<p>Le jardin avec l\'ID ' . htmlspecialchars($allJardinId) . ' n\'existe pas.</p>';
-        }
-    } catch (PDOException $e) {
-        echo "Erreur : " . $e->getMessage();
-    }
-} else {
-    echo '<p>Aucun ID du jardin fourni.</p>';
-}
-?>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-    <script src="../assets/vendor/jquery/jquery.min.js"></script>
-    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/js/script.js"></script>
+                                                    echo '<div class="mb-3"> <label for="site-title" class="form-label">Nombre de parcelles</label>
+                                                        <input class="form-control" type="text" name="jardin_n_parcelle" value="' . htmlspecialchars($allJardin['jardin_n_parcelle']) . '">
+                                                        </div><br>';
+
+                                                    echo '<div class="mb-3 text-end">
+                                                        <input class="btn btn-success" type="submit" value="Modifier">
+                                                        </div>';
+                                                    echo '</form>';
+                                                } else {
+                                                    echo '<p>Le jardin avec l\'ID ' . htmlspecialchars($allJardinId) . ' n\'existe pas.</p>';
+                                                }
+                                            } catch (PDOException $e) {
+                                                echo "Erreur : " . $e->getMessage();
+                                            }
+                                        } else {
+                                            echo '<p>Aucun ID du jardin fourni.</p>';
+                                        }
+                                        ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <script src="../assets/vendor/jquery/jquery.min.js"></script>
+            <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+            <script src="../assets/js/script.js"></script>
 </body>
 
 </html>
