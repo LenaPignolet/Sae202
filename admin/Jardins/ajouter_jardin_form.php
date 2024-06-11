f55bc0eb6461f900fb2cca8e32c7b94e8e139628
Basheer El Kurdi <112487394+basheer-11@users.noreply.github.com>
2024-06-11 16:04:55 +0200

    Update ajouter_jardin_form.php dashboard

--- admin/Jardins/ajouter_jardin_form.php
+++ admin/Jardins/ajouter_jardin_form.php
@@ -1,50 +1,124 @@
-<!DOCTYPE html>
+<!doctype html>
+<!-- 
+* Bootstrap Simple Admin Template
+* Version: 2.1
+* Author: Alexis Luna
+* Website: https://github.com/alexis-luna/bootstrap-simple-admin-template
+-->
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajouter un jardin</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
-    <link rel="icon" type="image/png" href="../images/icon.jpg">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Page admin">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dashboard</title>
+    <link href="../assets/vendor/fontawesome/css/fontawesome.min.css" rel="stylesheet">
+    <link href="../assets/vendor/fontawesome/css/solid.min.css" rel="stylesheet">
+    <link href="../assets/vendor/fontawesome/css/brands.min.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/css/master.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="/images/logo.png">
 </head>
 <body>
+  
+<div class="wrapper">
+        <nav id="sidebar" class="active">
+            <div class="sidebar-header">
+                <img src="../assets/img/logo.png" alt="bootraper logo" width="40px" class="app-logo">
+            </div>
+            <ul class="list-unstyled components text-secondary">
+                <li>
+                    <a href="/admin/gestion.php"><i class="fas fa-home"></i> Dashboard</a>
+                </li>
 
-<?php require('menu.php'); ?>
+                <li>
+                    <a href="gestion_jardin.php"><i class="fas fa-tree"></i> Gestion Jardins</a>
+                </li>
 
-    <a href="gestion_jardin.php">Retour au tableau de bord</a>
-    <hr>
-    <h1>Gestion de nos jardins</h1>
-    <p>Ajouter un nouveau jardin</p>
-    <hr>
-    <form method="POST" action="ajouter_jardin_valid.php" enctype="multipart/form-data">
-        Photo : <input type="file" name="photo"><br>
-        Nom : <input type="text" name="nom"><br>
-        Adresse : <input type="text" name="adresse"><br>
-        Surface : <input type="text" name="surface"><br>
-        Parcelle : 
-        <select name="numparcelle">
-            <?php
-            try {
-                $mabd = new PDO('mysql:host=localhost;dbname=sae202;charset=UTF8;', 'Usersae202', '123');
-                $mabd->query('SET NAMES utf8;');
-                $req = "SELECT * FROM Parcelle";
-                $resultat = $mabd->query($req);
+                <li>
+                    <a href="../Parcelle/gestion_parcelle.php"><i class="fas fa-chess-board"></i> Gestion Parcelles</a>
+                </li>
 
-                foreach ($resultat as $value) {
-                    echo '<option value="' . htmlspecialchars($value['parcelle_id']) . '">' . htmlspecialchars($value['parcelle_nom']) . '</option>';
-                }
-            } catch (PDOException $e) {
-                echo "Erreur de connexion : " . $e->getMessage();
-            }
-            ?>
-        </select>
+                <li>
+                    <a href="../Usagers/user_gestion.php"><i class="fas fa-user-friends"></i> Gestion Users</a>
+                </li>
+
+                <li>
+                    <a href="/index.php"><i class="fas fa-arrow-left"></i> Retour</a>
+                </li>
+
+            </ul>
+        </nav>
+        <div id="body" class="active">
+            <!-- navbar navigation component -->
+            <nav class="navbar navbar-expand-lg navbar-white bg-white">
+                <button title="menu" type="button" id="sidebarCollapse" class="btn btn-light">
+                    <i class="fas fa-bars"></i><span></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="nav navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <div class="nav-dropdown">
+                                <div class="dropdown-menu dropdown-menu-end nav-link-menu" aria-labelledby="nav1">
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- end of navbar navigation component -->
+<div class="content">
+                <div class="container">
+                    <div class="page-title">
+                        <h3>Ajouter un nouveau jardin</h3>
+                    </div>
+                    <div class="box box-primary">
+                        <div class="box-body">
+                            <div class="tab-content" id="myTabContent">
+                                <div class="tab-pane fade active show" id="general" role="tabpanel" aria-labelledby="general-tab">
+                                    <div class="col-md-6">
+                                    <form action="ajouter_jardin_valid.php" method="POST" enctype="multipart/form-data">
+        <div class="mb-3">
+        <label class="form-label">Choisir une photo</label>
+        <input id="formFile1" class="form-control" type="file" name="photo">
+        </div>
         <br>
-        <input type="submit" value="Ajouter">
+        <div class="mb-3">
+        <label class="form-label">Le nom du jardin</label>
+        <input class="form-control" type="text" name="nom">
+        </div>
+        <br>
+        <div class="mb-3">
+        <label class="form-label">Adresse</label>
+        <input class="form-control" type="text" name="adresse">
+        </div>
+        <br>
+        <div class="mb-3">
+        <label class="form-label">Surface (m²)</label>
+        <input class="form-control" type="text" name="surface">
+        </div>
+        <br>
+        <div class="mb-3">
+        <label class="form-label">Nombre de parcelle</label>
+        <input class="form-control" type="text" name="nParcelle">
+        </div>
+        <br>
+        <div class="mb-3 text-end">
+        <input class="btn btn-success" type="submit" name="submit" value="Ajouter">
+        </div>
     </form>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+ 
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+    <script src="../assets/vendor/jquery/jquery.min.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/script.js"></script>
 </body>
-<footer>
-    <p>© PAGEC - Tous droits réservés</p>
-    <a href="../mentions.php">Mentions légales</a>
-</footer>
+
 </html>
