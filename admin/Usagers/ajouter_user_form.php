14a8dff57e85f9de48d87ae4976400f1ccb15305
Basheer El Kurdi <112487394+basheer-11@users.noreply.github.com>
2024-06-13 08:19:11 +0200

    Update and rename ajouter.php to ajouter_user_form.php dashboard

---
+++ admin/Usagers/ajouter_user_form.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<!-- 
+* Bootstrap Simple Admin Template
+* Version: 2.1
+* Author: Alexis Luna
+* Website: https://github.com/alexis-luna/bootstrap-simple-admin-template
+-->
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Page admin">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dashboard</title>
+    <link href="../assets/vendor/fontawesome/css/fontawesome.min.css" rel="stylesheet">
+    <link href="../assets/vendor/fontawesome/css/solid.min.css" rel="stylesheet">
+    <link href="../assets/vendor/fontawesome/css/brands.min.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/css/master.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="/images/logo.png">
+</head>
+
+<body>
+    <div class="wrapper">
+        <nav id="sidebar" class="active">
+            <div class="sidebar-header">
+                <img src="../assets/img/logo.png" alt="bootraper logo" width="40px" class="app-logo">
+            </div>
+            <ul class="list-unstyled components text-secondary">
+                <li>
+                    <a href="/admin/gestion.php"><i class="fas fa-home"></i> Dashboard</a>
+                </li>
+                <li>
+                    <a href="../Jardins/gestion_jardin.php"><i class="fas fa-tree"></i> Gestion Jardins</a>
+                </li>
+                <li>
+                    <a href="../Parcelle/gestion_parcelle.php"><i class="fas fa-chess-board"></i> Gestion Parcelles</a>
+                </li>
+                <li>
+                    <a href="../Usagers/user_gestion.php"><i class="fas fa-user-friends"></i> Gestion Users</a>
+                </li>
+                <li>
+                    <a href="/index.php"><i class="fas fa-arrow-left"></i> Retour</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div id="body" class="active">
+            <!-- navbar navigation component -->
+            <nav class="navbar navbar-expand-lg navbar-white bg-white">
+                <button title="menu" type="button" id="sidebarCollapse" class="btn btn-light">
+                    <i class="fas fa-bars"></i><span></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="nav navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <div class="nav-dropdown">
+                                <div class="dropdown-menu dropdown-menu-end nav-link-menu" aria-labelledby="nav1">
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- end of navbar navigation component -->
+
+            <div class="content">
+                <div class="container">
+                    <div class="page-title">
+                        <h3>Ajouter un nouveau utilisateur</h3>
+                    </div>
+                    <div class="box box-primary">
+                        <div class="box-body">
+                            <div class="tab-content" id="myTabContent">
+                                <div class="tab-pane fade active show" id="general" role="tabpanel" aria-labelledby="general-tab">
+                                    <div class="col-md-6">
+                                        <form action="ajouter_user_valid.php" method="POST" enctype="multipart/form-data">
+                                            <div class="mb-3">
+                                                <label class="form-label">Choisir une photo</label>
+                                                <input id="formFile1" class="form-control" type="file" name="photo">
+                                            </div>
+                                            <br>
+                                            <div class="mb-3">
+                                                <label class="form-label">Nom</label>
+                                                <input class="form-control" type="text" name="nom">
+                                            </div>
+                                            <br>
+                                            <div class="mb-3">
+                                                <label class="form-label">Prénom</label>
+                                                <input class="form-control" type="text" name="prenom">
+                                            </div>
+                                            <br>
+                                            <div class="mb-3">
+                                                <label class="form-label">Mail</label>
+                                                <input class="form-control" type="text" name="mail">
+                                            </div>
+                                            <br>
+                                            <div class="mb-3">
+                                                <label class="form-label">Mot de passe</label>
+                                                <input class="form-control" type="text" name="mdp">
+                                            </div>
+                                            <br>
+                                            <div class="mb-3 text-end">
+                                                <input class="btn btn-success" type="submit" name="submit" value="Ajouter">
+                                            </div>
+                                        </form>
+                                    </div> <!-- Fin de .col-md-6 -->
+                                </div> <!-- Fin de .tab-pane -->
+                            </div> <!-- Fin de .tab-content -->
+                        </div> <!-- Fin de .box-body -->
+                    </div> <!-- Fin de .box -->
+                </div> <!-- Fin de .container -->
+            </div> <!-- Fin de .content -->
+        </div> <!-- Fin de #body -->
+    </div> <!-- Fin de .wrapper -->
+
+    <script src="../assets/vendor/jquery/jquery.min.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/script.js"></script>
+</body>
+
+</html>
